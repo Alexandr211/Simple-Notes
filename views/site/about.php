ce355f8769dc4371cdc2328887e3c1e6d017fddd
Alexandr211 <33276312+Alexandr211@users.noreply.github.com>
2018-11-19 13:11:09 +0700

    Update views/site/about.php

--- views/site/about.php
+++ views/site/about.php
@@ -12,7 +12,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <br><br>This service is developed by Alexander Bashtanov. <br><br>The following methods were used in the development:<br> 1. Authorization and storage of information using MySQL.<br> 2. The DB queries using various commands like SELECT, INSERT, JOIN, Migrations, Active record, etc.<br> 3. Users access to data is implemented on the basis of ACF and RBAC.<br> 4. Built-in controller to remove login information for CRON job scheduler (or run 'yii history/clear' in Console).<br><br> The product is free for use by anyone.<br><br><a href=#>You can getCode of the project from Github here.</a>
+        <br><br>This service is developed by Alexander Bashtanov. <br><br>The following methods were used in the development:<br> 1. Authorization and storage of information using MySQL.<br> 2. The DB queries using various commands like SELECT, INSERT, JOIN, Migrations, Active record, etc.<br> 3. Users access to data is implemented on the basis of ACF and RBAC.<br> 4. Built-in controller to remove login information for CRON job scheduler (or run 'yii history/clear' in Console).<br><br> The product is free for use by anyone.<br><br><a href="https://github.com/Alexandr211/Simple-Notes.git">You can getCode of the project from Github here.</a>
+        
         
     </p>
 
